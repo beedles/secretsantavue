e8d67ca7ec2c3aeb81cb2918b45aaa94664a63a0
Brendon Morgan <sire_2_bah@hotmail.com>
2018-01-25 21:17:47 +1300

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,90 @@
+/* eslint-disable no-unused-vars */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Test'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var todoapp = new Vue({
+  el: '#todo_app',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Cheds' }
+    ]
+  }
+})
+
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+var freezeapp = new Vue({
+  el: '#freeze_app',
+  data () {
+    return {
+      obj
+    }
+  }
+})
